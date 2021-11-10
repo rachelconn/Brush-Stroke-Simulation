6a28216d2bfd8d76566527c28a28f20f513769be
Ryan Conn <rconn478@gmail.com>
2021-11-10 10:08:54 -0600

    Use simplify-js to simplify stroke data

--- src/utils/exportExampleToSVG.ts
+++ src/utils/exportExampleToSVG.ts
@@ -1,15 +1,26 @@
 import fs from 'fs';
 import paper from 'paper';
-import { DrawingExample } from '../generate_training_data';
+import simplify from 'simplify-js';
+import { DrawingExample, StrokeData } from '../generate_training_data';
 
 if (!paper.project) paper.setup(new paper.Size(255, 255));
+
+function strokeDataToPath(strokeData: StrokeData): paper.Path {
+    return new paper.Path(strokeData.map((point) => new paper.Point(...point)));
+}
 
 export default function exportExampleToSVG(example: DrawingExample, filename: string) {
   paper.project.clear();
   example.strokes.forEach((stroke) => {
-    const path = new paper.Path(stroke.map((point) => new paper.Point(...point)));
+    const path = strokeDataToPath(stroke);
     path.strokeColor = new paper.Color('black');
     path.strokeWidth = 1;
+
+    // const simplified = simplifyPath(path, 0.95);
+    const simplified = strokeDataToPath(simplify(stroke, 5, true));
+    simplified.strokeColor = new paper.Color('#ff00ff');
+    simplified.strokeWidth = 1;
+    simplified.insertAbove(path);
   });
   const exported = paper.project.exportSVG({ asString: true }) as string;
 
